f0c234b85b1945614e54c855c8d648f0f59ac4f5
qishuixian <1960338340@qq.com>
2017-12-28 10:54:40 +0800

    通讯录系统

--- angular/README.md
+++
@@ -1,5 +0,0 @@
-
-#这是我平时练习angular项目的文件。。。。。
-
-#tell文件：
-这是一个使用angular-cli模仿书籍《揭秘Angular》通用例子的项目
--- angular/tell/src/app/shared/index.ts
+++
@@ -1,6 +0,0 @@
-// 在import的时候可以使用通配符*导入外部的模块
-export * from './btn-click.directive';
-export * from './util.service';
-export * from './footer.component';
-export * from './header.component';
-export * from './phone.pipe';
